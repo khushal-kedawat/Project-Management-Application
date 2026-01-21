9aa4b08663340e943001d8b5ca4e414c0afc6a43
khushal <khushal.kedawat@gmail.com>
2026-01-21 22:01:40 +0530

    Add mongoose hooks for hashing

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose , {Schema} from "mongoose";
-
+import bcrypt from "bcrypt";
+import { use } from "react";
 const userSchema = new Schema(
   {
     avatar:{
@@ -60,6 +61,18 @@ const userSchema = new Schema(
     timestamps: true
   },
 )
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
  export const User = mongoose.model("User",userSchema)
 
